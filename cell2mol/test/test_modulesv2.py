e7ca230256490d2fddf693490ed05b4c5d026fdb
rlaplaza <laplazasolanas@gmail.com>
2022-01-13 18:19:23 +0100

    Added YOXKUS and a preliminary test. It passes locally, but it is to be improved with assertion checks.

--- cell2mol/test/test_modulesv2.py
+++ cell2mol/test/test_modulesv2.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
+import os 
 # from cell2mol.module1 import test_addone, test_subtwo
 from cell2mol.module1 import addone, subtwo
-from cell2mol.cell2mol import cell2mol
+from cell2mol.cell2mol import cell2mol, split_infofile
 
 def test_modules1():
   assert addone(10) == 11
   assert subtwo(8) == 6 
 
 def test_cell2mol():
-  pass  
-#   cell2mol()
+  dir_path = os.path.dirname(os.path.realpath(__file__))
+  infofile = dir_path+"/infodata/YOXKUS.info"
+  refcode = split_infofile(infofile)
+  cell = cell2mol(infofile, refcode)
 
 # if __name__== "__main__":
 #   test_modules1() 
