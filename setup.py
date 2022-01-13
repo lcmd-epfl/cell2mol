a08088dae525fae46d17d09ce228cd0a2460d71a
= <=>
2022-01-13 15:00:38 +0100

    Initial setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import io
+
+# Read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with io.open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="cell2mol",
+    packages=["cell2mol"],
+    version="1.0",
+    description="Generator cell object from a cif file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="svela-bs, rlaplaza, choglass, lcmd-epfl",
+    url="https://github.com/lcmd-epfl/cell2mol/",
+    classifiers=["Programming Language :: Python :: 3"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "cell2mol = cell2mol:main",
+        ],
+    },
+)
