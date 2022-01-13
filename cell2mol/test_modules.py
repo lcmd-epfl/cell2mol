7b4be6cc3d98ce72bc5244222914a22d4a1facb7
= <=>
2022-01-13 14:16:59 +0100

    Update cell2mol

---
+++ cell2mol/test_modules.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# from cell2mol.module1 import test_addone, test_subtwo
+
+# test_addone()
+# test_subtwo()
+
+# if __name__== "__main__":
+
+#   test_addone()
+#   test_subtwo()
