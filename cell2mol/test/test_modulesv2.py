6689f69dc98e44055e06f5ed49585861a7b77f92
Yuri Cho <97517062+choglass@users.noreply.github.com>
2022-01-13 16:58:49 +0100

    Update test_modulesv2.py

--- cell2mol/test/test_modulesv2.py
+++ cell2mol/test/test_modulesv2.py
@@ -2,12 +2,13 @@
 
 # from cell2mol.module1 import test_addone, test_subtwo
 from cell2mol.module1 import addone, subtwo
+from cell2mol.cell2mol import cell2mol
 
 def test_modules1():
   assert addone(10) == 11
   assert subtwo(8) == 6 
 
-# def test_cell2mol ():
+def test_cell2mol ():
   
 #   cell2mol()
 
