d943d404753e6478a4571aa522c9f50ee9adbbe9
Yuri Cho <97517062+choglass@users.noreply.github.com>
2022-01-14 16:40:36 +0100

    Update test_pickle.py

--- cell2mol/test/test_pickle.py
+++ cell2mol/test/test_pickle.py
@@ -5,8 +5,9 @@ import os
 import numpy as np
 import pickle
 
+import cell2mol.tmcharge_common
 from cell2mol.cell2mol import cell2mol, split_infofile
-import cell2mol.tmcharge_common 
+ 
 
 def test_cell2mol():
     dir_path = os.path.dirname(os.path.realpath(__file__))
