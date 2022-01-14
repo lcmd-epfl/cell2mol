0aace2b7daf5b242a575b61c507f94b88f97c95d
= <=>
2022-01-14 14:52:28 +0100

    YOXKUS test

---
+++ cell2mol/test/test_cell.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from array import array
+import os
+import numpy as np
+import pickle
+
+from cell2mol.cell2mol import cell2mol, split_infofile
+
+def test_cell2mol():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    infofile = "YOXKUS.info"
+    infopath = dir_path + "/infodata/" + infofile
+    refcode = split_infofile(infofile)
+    cell = cell2mol(infopath, refcode)
+    print(
+        "=====================================pytest====================================="
+    )
+    print(cell.version)
+    print(cell.refcode)
+    print(cell.cellvec)
+    print(cell.cellparam)
+    print(cell.labels)
+    print(cell.pos)
+
+
+def test_check_cell():
+    
+    cell = test_cell2mol()
+    
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    cellpath = dir_path + "/infodata" 
+    file = open(f"{output_dir}/Cell_{cell.refcode}.gmol",'rb')
+    result = pickle.load(file)
+    print(result.version)
+    print(result.refcode)
+    print(result.cellvec)
+    print(result.cellparam)
+    print(result.labels)
+    print(result.pos)
+    assert cell.version == result.version
+    assert cell.refcode == result.refcode
+    assert cell.cellvec == result.cellvec
+    assert cell.cellparam == result.cellparam
+    assert cell.labels == result.labels
+    assert np.allclose(cell.pos, result.pos)
+    assert cell.warning_list == result.warning_list
