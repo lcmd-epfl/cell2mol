8d037eed813408712a9c7a7ab944288a5bdb166a
= <=>
2022-01-14 15:33:14 +0100

    test

--- cell2mol/test/test_cell.py
+++ cell2mol/test/test_cell.py
@@ -23,23 +23,24 @@ def test_cell2mol():
     print(cell.cellparam)
     print(cell.labels)
     print(cell.pos)
-
+    
+    return cell
 
 def test_check_cell():
     
-    # cell = test_cell2mol()
+    cell = test_cell2mol()
     
-    # dir_path = os.path.dirname(os.path.realpath(__file__))
-    # cellpath = dir_path + "/infodata" 
-    # file = open(f"{cellpath}/Cell_{cell.refcode}.gmol",'rb')
-    # result = pickle.load(file)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    cellpath = dir_path + "/infodata" 
+    file = open(f"{cellpath}/Cell_{cell.refcode}.gmol",'rb')
+    result = pickle.load(file)
     
-    # print(result.version)
-    # print(result.refcode)
-    # print(result.cellvec)
-    # print(result.cellparam)
-    # print(result.labels)
-    # print(result.pos)
+    print(result.version)
+    print(result.refcode)
+    print(result.cellvec)
+    print(result.cellparam)
+    print(result.labels)
+    print(result.pos)
     pass
 
     # assert cell.version == result.version
