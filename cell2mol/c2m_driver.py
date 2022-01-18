87a3ab02fa0dff359d0c49e00d3bcaabfd40ae0d
= <=>
2022-01-18 19:43:38 +0100

    update

--- cell2mol/c2m_driver.py
+++ cell2mol/c2m_driver.py
@@ -8,30 +8,31 @@ import pickle
 # Import modules
 from cell2mol.c2m_module import split_infofile, save_cell, cell2mol
 from cell2mol.cif2info import cif_2_info
+
 if __name__ == "__main__" or __name__ == "cell2mol.c2m_driver":
     print("Running!")
 
     pwd = os.getcwd()
-    
+
     infofile, step = parsing_arguments()
-    
+
     root, extension = os.path.splitext(infofile)
     refcode = split_infofile(infofile)
-    
+
     # If infofile is a .cif file
-    if extension == '.cif':    
+    if extension == ".cif":
         new_info_file = "{}.info".format(refcode)
         sys.stdout = open(new_info_file, "w")
-        cif_2_info (infofile)
+        cif_2_info(infofile)
         sys.stdout.close()
         infopath = pwd + "/" + new_info_file
-        
-    # If infofile is a .info file       
-    elif extension == '.info':
+
+    # If infofile is a .info file
+    elif extension == ".info":
         infopath = pwd + "/" + infofile
-    
-    else :
-        print ("Wrong Input File")
+
+    else:
+        print("Wrong Input File")
 
     # dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -44,13 +45,13 @@ if __name__ == "__main__" or __name__ == "cell2mol.c2m_driver":
 
     if step == 1 or step == 3:
         sys.stdout = open(output_fname, "w")
-    elif  step == 2 :
+    elif step == 2:
         sys.stdout = open(output_fname, "a")
-        file = open(f"{output_dir}/Cell_{refcode}.gmol",'rb')
+        file = open(f"{output_dir}/Cell_{refcode}.gmol", "rb")
         cell = pickle.load(file)
-    else : 
-         print("Inproper step number")
-    
+    else:
+        print("Inproper step number")
+
     cell = cell2mol(infopath, refcode, output_dir, step)
     print_types = "gmol"
     save_cell(cell, print_types, output_dir)
