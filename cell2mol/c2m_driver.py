8439ad8bdad53982ab00dbd45e966a52a0886083
= <=>
2022-01-19 13:49:21 +0100

    Update c2m_driver c2m_module

--- cell2mol/c2m_driver.py
+++ cell2mol/c2m_driver.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import pickle
+import time
 
 # Import modules
 from cell2mol.helper import parsing_arguments
@@ -26,65 +27,67 @@ if __name__ == "__main__" or __name__ == "cell2mol.c2m_driver":
     root, extension = os.path.splitext(infofile)
     refcode = split_infofile(infofile)
 
-    # If infofile is a .cif file
-    if extension == ".cif":
-        print("Convert .cif file to .info file")
-        new_info_file = "{}.info".format(refcode)
-        sys.stdout = open(new_info_file, "w")
-        cif_2_info()
-        sys.stdout.close()
-        infopath = pwd + "/" + new_info_file
-    # If infofile is a .info file
-    elif extension == ".info":
-        infopath = pwd + "/" + infofile
+    if step == 2:
+        infopath = None
 
-    else:
-        print("Wrong Input File Format")
-        sys.exit(1)
-
-    if infopath:
-        output_dir = pwd + "/" + refcode
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-        
-        # import cell object in case of step 2
-        if step == 2: 
-            existing_cell = output_dir + "/" + "Cell_{}.gmol".format(refcode)
-            if os.path.isfile(existing_cell):
-                file = open(existing_cell, "rb")
-                cell = pickle.load(file)
-            else:
-                print(f"No cell object in outpud directory: {existing_cell}")
+    elif step == 1 or step == 3:
+        # If infofile is a .cif file
+        if extension == ".cif":
+            print("Convert .cif file to .info file")
+            new_info_file = "{}.info".format(refcode)
+            sys.stdout = open(new_info_file, "w")
+            cif_2_info()
+            sys.stdout.close()
+            infopath = pwd + "/" + new_info_file
+        # If infofile is a .info file
+        elif extension == ".info":
+            infopath = pwd + "/" + infofile
+            if not os.path.exists(infopath):
+                print(f"Error: The file {infofile} could not be found.\n")
                 sys.exit(1)
-        else :
-            pass
-            
-
-        # save output and cell object
-        output_fname = output_dir + "/output.out"
-
-        if step == 1 or step == 3:
-            sys.stdout = open(output_fname, "w")
-        elif step == 2:
-            sys.stdout = open(output_fname, "a")
-
-        cell = cell2mol(infopath, refcode, output_dir, step)
-        print_types = "gmol"
-        save_cell(cell, print_types, output_dir)
-        sys.stdout.close()
-
-        # save error
-        res = [i for i, val in enumerate(cell.warning_list) if val]
-        if len(res) == 0:
-            error_code = 0
         else:
-            for i in res:
-                error_code = i + 1
+            print("Wrong Input File Format")
+            sys.exit(1)
 
-        error_fname = output_dir + "/error_{}.out".format(error_code)
-        sys.stdout = open(error_fname, "w")
-        print(cell.print_Warning())
-        sys.stdout.close()
+    output_dir = pwd + "/" + refcode
+
+    if step == 2:
+        cellpath = output_dir + "/Cell_{}.gmol".format(refcode)
+        filename = str(output_dir) + "/" + "Cell_" + str(refcode) + ".gmol"
+        if os.path.exists(cellpath):
+            print("Cell object exist in output directory")
+        else:
+            print("No such file exists {}".format(filename))
+            sys.exit(1)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # save output and cell object
+    output_fname = output_dir + "/output.out"
+
+    if step == 1 or step == 3:
+        sys.stdout = open(output_fname, "w")
+    elif step == 2:
+        sys.stdout = open(output_fname, "a")
+
+    cell = cell2mol(infopath, refcode, output_dir, step)
+    print_types = "gmol"
+    save_cell(cell, print_types, output_dir)
+    sys.stdout.close()
+
+    # save error
+    res = [i for i, val in enumerate(cell.warning_list) if val]
+    if len(res) == 0:
+        error_code = 0
     else:
-        print("Error!")
-        sys.exit(1)
+        for i in res:
+            error_code = i + 1
+
+    error_fname = output_dir + "/error_{}.out".format(error_code)
+    sys.stdout = open(error_fname, "w")
+    print(cell.print_Warning())
+    sys.stdout.close()
+else:
+    print("Error!")
+    sys.exit(1)
