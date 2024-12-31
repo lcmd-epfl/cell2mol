65b561c9bf76eb0ca0d8fea6a7723fa4cb244b62
choglass <yuri.cho@epfl.ch>
2024-12-31 12:58:26 +0100

    handling error exit_with_error_exception

--- cell2mol/final_c2m_driver.py
+++ cell2mol/final_c2m_driver.py
@@ -4,7 +4,7 @@ from cell2mol.helper import parsing_arguments
 from cell2mol.refcell import process_refcell
 from cell2mol.unitcell import process_unitcell
 from cell2mol.xyz_molecule import get_molecule
-from cell2mol.read_write import prefilter_cif, exit_with_error
+from cell2mol.read_write import prefilter_cif, exit_with_error_input, exit_with_error_exception
 
 def main():
     input, system_type, cell_para, debug_mode = parsing_arguments()
@@ -15,32 +15,35 @@ def main():
 
     print(input, input_path, system_type, cell_para, debug_mode, name, extension)
     if not os.path.exists(input_path):
-        exit_with_error(f"Input file not found: {input_path}")
+        exit_with_error_input(f"Input file not found: {input_path}")
     if extension == ".cif":
         if prefilter_cif(input_path):
             handle_cif_file(input_path, system_type, name, current_dir, debug_mode)
         else:
-            exit_with_error("CIF file is not suitable for processing")
+            exit_with_error_input("CIF file is not suitable for processing")
     elif extension == ".xyz":
         handle_xyz_file(input_path, system_type, name, cell_para, current_dir, debug_mode)
     else:
-        exit_with_error("Invalid file extension")
+        exit_with_error_input("Invalid file extension")
 
 def handle_cif_file(input_path, system_type, name, current_dir, debug_mode):
     if system_type == "reference":
         print("Processing reference (Wyckoff sites) from .cif file")
-        process_refcell(input_path, name, current_dir, debug_mode)
+        try:
+            process_refcell(input_path, name, current_dir, debug_mode)
+        except Exception as e:
+            exit_with_error_exception(e)
     elif system_type == "unitcell":
         print("Processing unit cell from .cif file")
         process_unitcell(input_path, name, current_dir, debug_mode)
     else:
-        exit_with_error("Invalid system type for .cif file", {"system_type": system_type})
+        exit_with_error_input("Invalid system type for .cif file", {"system_type": system_type})
 
 
 def handle_xyz_file(input_path, system_type, name, cell_para, current_dir, debug_mode):
     if system_type == "unitcell":
         if cell_para is None:
-            exit_with_error("Cell parameters must be provided for .xyz file of a unit cell")
+            exit_with_error_input("Cell parameters must be provided for .xyz file of a unit cell")
         else:
             print("Processing unit cell from .xyz file")
             # users should provide chemical formula (Fe-O2-H3) of refmoleculist
@@ -49,7 +52,7 @@ def handle_xyz_file(input_path, system_type, name, cell_para, current_dir, debug
         print("Processing molecule from .xyz file")
         get_molecule(input_path, name, current_dir, debug_mode)
     else:
-        exit_with_error("Invalid system type for .xyz file", {"system_type": system_type})
+        exit_with_error_input("Invalid system type for .xyz file", {"system_type": system_type})
 
 
 if __name__ == "__main__" or __name__ == "cell2mol.final_c2m_driver":
