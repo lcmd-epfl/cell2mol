a14af92a0370fa3e41ccbd4e6a5ee8670ae04f39
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2025-10-16 15:10:29 +0200

    Update imports in __main__.py to include final_c2m_driver, improving module accessibility and code organization.

--- cell2mol/__main__.py
+++ cell2mol/__main__.py
@@ -3,8 +3,10 @@ from __future__ import absolute_import
 
 import os
 import sys
+
 # from cell2mol import c2m_driver
 # from cell2mol import new_c2m_driver
+from cell2mol import final_c2m_driver  # noqa: F401
 
 if __package__ == "":
     path = os.path.dirname(os.path.dirname(__file__))
