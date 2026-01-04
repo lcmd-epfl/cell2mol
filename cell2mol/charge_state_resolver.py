7ca210c07466883b5c574417c570fb18df4a97cb
my-name <yuri.cho@epfl.ch>
2026-01-04 11:57:13 +0100

    refactor: remove Specie parameter from enumerate_charge_states function feat: add ProtonationGroupResult to type registry

--- cell2mol/charge_state_resolver.py
+++ cell2mol/charge_state_resolver.py
@@ -1,10 +1,9 @@
 from cell2mol.classes.charge_state import ChargeState
-from cell2mol.classes.specie import Specie
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-def enumerate_charge_states(specie: Specie) -> list[ChargeState]:
+def enumerate_charge_states() -> list[ChargeState]:
     """Enumerate possible charge states for the specie."""
     return
