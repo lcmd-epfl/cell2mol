c4ef6823f4caf3e0fb7245723703f748a9d2b600
my-name <yuri.cho@epfl.ch>
2026-01-16 20:08:44 +0100

    saving unit cell data

--- cell2mol/unitcell.py
+++ cell2mol/unitcell.py
@@ -83,7 +83,7 @@ def interpret_unitcell(input_path, name, current_dir):
     cells.unitcell = unitcell
 
     cells.save(os.path.join(current_dir, f"Cells_{name}.json"), format="json")
-    # cells.save(os.path.join(current_dir, f"Cells_{name}.cell"), format="pickle")
+    cells.save(os.path.join(current_dir, f"Cells_{name}.cell"), format="pickle")
 
     # Summary
     summary_fname = os.path.join(current_dir, "reference_summary.out")
