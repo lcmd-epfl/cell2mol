0a7167e832b284ff0f44f1870c1a560c558d236b
my-name <yuri.cho@epfl.ch>
2026-01-16 19:31:08 +0100

    Remove charge assignment modules

--- cell2mol/charge_state_resolver.py
+++
@@ -1,9 +0,0 @@
-from cell2mol.classes.charge_state import ChargeState
-import logging
-
-logger = logging.getLogger(__name__)
-
-
-def enumerate_charge_states() -> list[ChargeState]:
-    """Enumerate possible charge states for the specie."""
-    return
