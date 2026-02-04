dac961c1cb7d3d2fdbd88b2ae3dd8d50bbe54512
my-name <yuri.cho@epfl.ch>
2026-02-04 21:10:38 +0100

    feat(protonation): update Protonation class to improve proton handling and update attributes

--- cell2mol/classes/protonation.py
+++ cell2mol/classes/protonation.py
@@ -5,10 +5,8 @@ from cell2mol.connectivity import get_adjmatrix, get_adjmatrix_from_cif_bonds
 from cell2mol.element_utils import labels2formula, get_radii
 from cell2mol.utils.pydantic import BaseModel
 from cell2mol.my_types import NDArray
-
-# Pydantic imports for the converted classes
 from pydantic import Field, computed_field
-from typing_extensions import deprecated
+from typing_extensions import Literal, deprecated
 from cell2mol.elementdata import ElementData
 import logging
 from cell2mol.utils import config
@@ -20,18 +18,24 @@ elemdatabase = ElementData()
 class Protonation(BaseModel):
     # Required constructor parameters
     labels: list[str]
-    coords: NDArray  # Note: renamed from 'coord' to match usage
+    coord: NDArray
     cov_factor: float
-    added_atoms: int
-    addedlist: list[int]
-    block: list[int]
-    metal_electrons: list[int]
-    elemlist: list[str]
 
-    # Optional constructor parameters with defaults
-    tmpsmiles: str = Field(default=" ")
-    o_s: int = Field(default=0)
-    typ: str = Field(default="Local")
+    # Total number of protons added
+    n_protons_added: int
+
+    # Protons added per site (len = length of parent labels)
+    site_proton_counts: list[int]
+
+    # Electrons donated from ligand lone pairs per site (len = length of parent labels)
+    ligand_donor_electrons: list[int]
+
+    # Protonation mode
+    # "none" : no protonation
+    # "heuristic": apply chemical rules
+    # "combinatorial": explore combinations
+    mode: str | Literal["none", "heuristic", "combinatorial"] | None = None
+
     parent: object | None = Field(default=None)
 
     # Computed attributes with proper defaults
@@ -99,18 +103,36 @@ class Protonation(BaseModel):
                 self.status = True
                 adjmat = get_adjmatrix_from_cif_bonds(
                     self.labels,
-                    self.coords,
+                    self.coord,
                     atom_site_labels=self.atom_site_labels,
                     bond_data=bond_data,
                 )
                 adjnum = adjmat.sum(axis=1)
-                count = 0
-                if len(self.addedlist) > 0:
-                    for idx, add in enumerate(self.addedlist):
-                        if add != 0:
-                            count += 1
-                            added_idx = len(self.addedlist) - 1 + count
-                            logger.debug("PROTONATION.added_idx %s %s", idx, added_idx)
+                base_count = len(self.labels) - self.n_protons_added
+                proton_count = 0
+                for idx, n_added in enumerate(self.site_proton_counts):
+                    if n_added > 0:
+                        coordinating_atom_label = (
+                            f"{self.labels[idx]} ({self.atom_site_labels[idx]})"
+                            if self.atom_site_labels
+                            else self.labels[idx]
+                        )
+                        logger.debug(
+                            "Coordinating atom site (idx: %d) %s %s add %s protons",
+                            idx,
+                            coordinating_atom_label,
+                            self.coord[idx],
+                            n_added,
+                        )
+                        for _ in range(n_added):
+                            added_idx = base_count + proton_count
+                            proton_count += 1
+                            logger.debug(
+                                "     Added proton site (idx: %d) %s %s",
+                                added_idx,
+                                self.labels[added_idx],
+                                self.coord[added_idx],
+                            )
                             adjmat[idx, added_idx] += 1
                             adjmat[added_idx, idx] += 1
                             adjnum[idx] += 1
@@ -122,81 +144,79 @@ class Protonation(BaseModel):
                 # 1. Generate the initial adjacency matrix
                 self.status, self.adjmat, warning = get_adjmatrix(
                     self.labels,
-                    self.coords,
+                    self.coord,
                     radii=self.radii,
                     cov_factor=self.cov_factor,
-                    add_atom=True,
+                    # add_atom=True,  # Not needed since we manually fix connectivity below
                 )
 
                 # 2. Synchronize adjnum with the new matrix
                 self.adjnum = self.adjmat.sum(axis=1)
 
-                # 3. Fix connectivity for unconditionally added atoms
+                # 3. Fix connectivity for unconditionally added protons
+                base_count = len(self.labels) - self.n_protons_added
+                proton_count = 0
+                for idx, n_added in enumerate(self.site_proton_counts):
+                    if n_added > 0:
+                        coordinating_atom_label = (
+                            f"{self.labels[idx]} ({self.atom_site_labels[idx]})"
+                            if self.atom_site_labels
+                            else self.labels[idx]
+                        )
+                        logger.debug(
+                            "Coordinating atom site (idx: %d) %s %s add %s protons",
+                            idx,
+                            coordinating_atom_label,
+                            self.coord[idx],
+                            n_added,
+                        )
 
-                if len(self.addedlist) > 0:
-                    # Identify the starting index of the newly added atoms
-                    # If self.labels grew by the number of non-zero entries in addedlist:
-                    original_count = len(self.labels) - sum(
-                        1 for x in self.addedlist if x != 0
-                    )
-
-                    current_added_offset = 0
-                    for original_site_idx, add_flag in enumerate(self.addedlist):
-                        if add_flag != 0:
+                        for _ in range(n_added):
                             # Calculate the absolute index of the added atom in the current matrix
-                            added_atom_idx = original_count + current_added_offset
-                            current_added_offset += 1
+                            added_idx = base_count + proton_count
+                            proton_count += 1
                             logger.debug(
-                                "Enforcing single bond: Site %d %s%s %s <-> Added Atom %d %s %s",
-                                original_site_idx,
-                                self.labels[original_site_idx],
-                                f" ({self.atom_site_labels[original_site_idx]})"
-                                if self.atom_site_labels
-                                else "",
-                                self.coords[original_site_idx],
-                                added_atom_idx,
-                                self.labels[added_atom_idx],
-                                # f" ({self.atom_site_labels[added_atom_idx]})"
-                                # if self.atom_site_labels
-                                # else "",
-                                self.coords[added_atom_idx],
+                                "     Added proton site (idx: %d) %s %s",
+                                added_idx,
+                                self.labels[added_idx],
+                                self.coord[added_idx],
                             )
 
                             # Clear all 'accidental' bonds for the added atom
-                            self.adjmat[added_atom_idx, :] = 0
-                            self.adjmat[:, added_atom_idx] = 0
+                            self.adjmat[added_idx, :] = 0
+                            self.adjmat[:, added_idx] = 0
 
                             # Enforce the single intended bond
-                            self.adjmat[original_site_idx, added_atom_idx] = 1
-                            self.adjmat[added_atom_idx, original_site_idx] = 1
+                            self.adjmat[idx, added_idx] = 1
+                            self.adjmat[added_idx, idx] = 1
 
-                    # 4. Final Recalculation: Always recalculate adjnum after manual adjmat changes
-                    self.adjnum = self.adjmat.sum(axis=1)
+                # 4. Final Recalculation: Always recalculate adjnum after manual adjmat changes
+                self.adjnum = self.adjmat.sum(axis=1)
 
-                if warning:
-                    self.status = False
+            # if warning:
+            #     self.status = False
 
     def reorder(self, map):
-        ## for protonation states with added atoms, the reorder map will have fewer items. Correct it here
+        ## for protonation states with added protons, the reorder map will have fewer items. Correct it here
         mapext = np.copy(map)
-        if self.added_atoms > 0 and len(map) < len(self.labels):
-            for ldx in range(0, self.added_atoms):
+        if self.n_protons_added > 0 and len(map) < len(self.labels):
+            for ldx in range(0, self.n_protons_added):
                 mapext = np.append(mapext, len(map) + ldx)
 
         assert len(mapext) == len(self.labels)
-        assert len(map) == len(self.addedlist)
+        assert len(map) == len(self.site_proton_counts)
         if len(map) > 0:
             self.labels = list(np.array(self.labels)[mapext])
-            self.coords = list(np.array(self.coords)[mapext])
+            self.coord = list(np.array(self.coord)[mapext])
             self.atnums = list(np.array(self.atnums)[mapext])
             self.radii = list(np.array(self.radii)[mapext])
             # No more hasattr check needed - atom_site_labels is always defined (can be None)
             if self.atom_site_labels is not None:
                 self.atom_site_labels = list(np.array(self.atom_site_labels)[map])
-            self.addedlist = list(np.array(self.addedlist)[map])
-            self.block = list(np.array(self.block)[map])
-            self.metal_electrons = list(np.array(self.metal_electrons)[map])
-            self.elemlist = list(np.array(self.elemlist)[map])
+            self.site_proton_counts = list(np.array(self.site_proton_counts)[map])
+            self.ligand_donor_electrons = list(
+                np.array(self.ligand_donor_electrons)[map]
+            )
 
             self.typ = "Reordered"
             refcell = self.parent.get_parent("reference")
@@ -206,17 +226,17 @@ class Protonation(BaseModel):
                 self.status = True
                 adjmat = get_adjmatrix_from_cif_bonds(
                     self.labels,
-                    self.coords,
+                    self.coord,
                     atom_site_labels=self.atom_site_labels,
                     bond_data=bond_data,
                 )
                 adjnum = adjmat.sum(axis=1)
                 count = 0
-                if len(self.addedlist) > 0:
-                    for idx, add in enumerate(self.addedlist):
-                        if add != 0:
+                if len(self.site_proton_counts) > 0:
+                    for idx, n_added in enumerate(self.site_proton_counts):
+                        if n_added != 0:
                             count += 1
-                            added_idx = len(self.addedlist) - 1 + count
+                            added_idx = len(self.site_proton_counts) - 1 + count
                             adjmat[idx, added_idx] += 1
                             adjmat[added_idx, idx] += 1
                             adjnum[idx] += 1
@@ -227,7 +247,7 @@ class Protonation(BaseModel):
             else:
                 self.status, self.adjmat, warning = get_adjmatrix(
                     self.labels,
-                    self.coords,
+                    self.coord,
                     radii=self.radii,
                     cov_factor=self.cov_factor,
                 )
@@ -246,9 +266,14 @@ class Protonation(BaseModel):
         # No more hasattr check needed - atom_site_labels is always defined (can be None)
         if self.atom_site_labels is not None:
             to_print += f" Atom site labels                = {self.atom_site_labels}\n"
-        to_print += f" Type                            = {self.typ}\n"
-        to_print += f" Atoms added in positions        = {self.addedlist}\n"
-        to_print += f" Atoms blocked (no atoms added)  = {self.block}\n"
+        to_print += f" Type                            = {self.type}\n"
+        to_print += f" Protonation Mode                = {self.mode}\n"
+        to_print += f" Number of added protons         = {self.n_protons_added}\n"
+        to_print += f" Protons added in positions      = {self.site_proton_counts}\n"
+        if sum(self.ligand_donor_electrons) > 0:
+            to_print += (
+                f" Ligand donor electrons          = {self.ligand_donor_electrons}\n"
+            )
         to_print += "---------------------------------------------------\n"
         return to_print
 
@@ -257,29 +282,21 @@ class Protonation(BaseModel):
     def from_positional(
         cls,
         labels: list[str],
-        coord: list[list[float]],
+        coord: NDArray,
         cov_factor: float,
-        added_atoms: int,
-        addedlist: list[int],
-        block: list[int],
-        metal_electrons: list[int],
-        elemlist: list[str],
-        tmpsmiles: str = " ",
-        o_s: int = 0,
-        typ: str = "Local",
+        n_protons_added: int,
+        site_proton_counts: list[int],
+        ligand_donor_electrons: list[int],
+        mode: str = None,
         parent: object = None,
     ) -> "Protonation":
         return cls(
             labels=labels,
-            coords=coord,  # Note: using coords here to match the field name
+            coord=coord,
             cov_factor=cov_factor,
-            added_atoms=added_atoms,
-            addedlist=addedlist,
-            block=block,
-            metal_electrons=metal_electrons,
-            elemlist=elemlist,
-            tmpsmiles=tmpsmiles,
-            o_s=o_s,
-            typ=typ,
+            n_protons_added=n_protons_added,
+            site_proton_counts=site_proton_counts,
+            ligand_donor_electrons=ligand_donor_electrons,
+            mode=mode,
             parent=parent,
         )
